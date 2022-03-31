a9ec490f6507705224e1b1024ed56e5359110bba
michal34 <86197538+michal34@users.noreply.github.com>
2022-03-31 11:57:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.getElementById("container");
-const url = "https://dog.ceo/api/breeds/image/random";
+const url = "https://herokunodeapp12.herokuapp.com/";
 
 async function funcName(url) {
   const response = await fetch(url);
-  var data = await response.json();
-  console.log(data.message);
-  container.innerHTML = `<img src="${data.message}">`;
+  let data = await response.json();
+  console.log(data);
+  container.innerHTML = "data";
 }
 
 funcName(url);
