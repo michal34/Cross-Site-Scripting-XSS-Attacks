365ff5fe32f6a9e3ef0ee8c914b9be581761033a
michal34 <86197538+michal34@users.noreply.github.com>
2022-04-01 13:20:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,82 @@
-const container = document.getElementById("container");
-const url = "https://herokunodeapp12.herokuapp.com/";
+const userMessages = [];
+let userMessages2 = {};
 
-async function funcName(url) {
-  const response = await fetch(url);
-  let data = await response.json();
-  console.log(data);
-  container.innerHTML = data[0];
+const userMessageForm = document.querySelector("form");
+const userMessagesList = document.querySelector("ul");
+
+function renderMessages() {
+  let messageItems = "";
+  for (const message of userMessages) {
+    messageItems = `
+    ${messageItems}
+    <li class="message-item">
+      <div class="message-image">
+        <img src="${message.image}" alt="${message.text}">
+    </div>
+    <p>${message.text}</p>
+    </li>
+    `;
+  }
+
+  userMessagesList.innerHTML = messageItems;
 }
 
-funcName(url);
+function formSubmitHandler(event) {
+  event.preventDefault();
+  const userMessageInput = event.target.querySelector("textarea");
+  const messageImageInput = event.target.querySelector("input");
+  const userMessage = userMessageInput.value;
+  const imageUrl = messageImageInput.value;
+
+  if (
+    !userMessage ||
+    !imageUrl ||
+    userMessage.trim().length === 0 ||
+    imageUrl.trim().length === 0
+  ) {
+    alert("Please insert a valid message and image.");
+    return;
+  }
+
+  userMessages.push({
+    text: userMessage,
+    image: imageUrl,
+  });
+
+  userMessages2 = { text: userMessage, image: imageUrl };
+
+  userMessageInput.value = "";
+  messageImageInput.value = "";
+
+  renderMessages();
+  addData();
+}
+
+userMessageForm.addEventListener("submit", formSubmitHandler);
+
+const fetchData = () => {
+  fetch("https://herokunodeapp12.herokuapp.com/data")
+    .then((response) => response.json())
+    .then((data) => {
+      data.data.map((dat) => {
+        userMessages.push({
+          text: dat.text,
+          image: dat.img,
+        });
+      });
+      renderMessages();
+      console.log(userMessages);
+    });
+};
+
+const addData = () => {
+  const data = { userMessages2 };
+  fetch("http://localhost:3000/wyslij/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+};
+fetchData();
